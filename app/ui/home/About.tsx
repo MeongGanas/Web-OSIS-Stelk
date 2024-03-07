2087cd3745aa015c0324409833d5745712944ca2
MeongGanas <farreluken@gmail.com>
2024-03-07 16:56:26 +0800

    menambah foto pada halaman about

--- app/ui/home/About.tsx
+++ app/ui/home/About.tsx
@@ -7,13 +7,13 @@ const AboutUs = () => {
     <div className="relative min-h-screen">
       <div className="w-full shapebg h-full">
         <div className="container w-full h-full flex items-center relative py-40 text-white">
-          <div>
+          <div className="pt-40">
             <h1 className="text-2xl font-extrabold uppercase mb-10">
               About Us
             </h1>
-            <div className="flex flex-wrap justify-evenly">
-              <div className="w-full sm:w-1/2 pr-5">
-                <h2 className={`text-2xl mb-3 ${satisfy.className}`}>Visi</h2>
+            <div className="flex flex-wrap justify-evenly items-center">
+              <div className="w-full md:w-1/2 pr-5">
+                <h2 className={`text-2xl mb-5 ${satisfy.className}`}>Visi</h2>
                 <p>
                   Menjadikan OSIS SMK Telkom Makassar sebagai wadah yang
                   menampung segala aspirasi, bakat, potensi dan kreativitas
@@ -22,12 +22,12 @@ const AboutUs = () => {
                   mengedepankan Iman dan Taqwa (IMTAQ).
                 </p>
               </div>
-              <div className="w-full md:w-1/2 bg-white text-black flex items-center justify-center rounded-md mt-5 md:mt-0">
-                <h1>Foto Bersama</h1>
+              <div className="w-full md:w-1/2 overflow-hidden text-black flex items-center justify-center rounded-md mt-5 md:mt-0">
+                <img src="/images/raker.jpg" alt="" />
               </div>
             </div>
-            <div className="flex flex-wrap mt-10 justify-evenly">
-              <div className="w-full sm:w-1/2 pr-5">
+            <div className="flex flex-wrap mt-10 justify-evenly items-center">
+              <div className="w-full md:w-1/2 pl-5 md:order-2">
                 <h2 className={`text-2xl mb-3 ${satisfy.className}`}>Misi</h2>
                 <ol className="pl-5 list-decimal text-sm md:text-base">
                   <li>
@@ -59,8 +59,8 @@ const AboutUs = () => {
                   </li>
                 </ol>
               </div>
-              <div className="w-full md:w-1/2 bg-white text-black flex items-center justify-center rounded-md mt-5 md:mt-0">
-                <h1>Foto Bersama</h1>
+              <div className="w-full md:w-1/2 bg-white h-fit overflow-hidden md:order-1 text-black flex items-center justify-center rounded-md mt-5 md:mt-0">
+                <img src="/images/raker.jpg" alt="" />
               </div>
             </div>
           </div>
