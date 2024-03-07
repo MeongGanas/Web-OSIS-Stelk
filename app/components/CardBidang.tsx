022922eeb232e3698fe54652e185811dbda49857
MeongGanas <farreluken@gmail.com>
2024-03-07 16:38:18 +0800

    menambah list bidang

---
+++ app/components/CardBidang.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const CardBidang = ({ url, name }: { name: string; url: string }) => {
+  return (
+    <Link
+      href={""}
+      className="relative rounded-md overflow-hidden hover:scale-105 transition duration-200 "
+    >
+      <div className="bg-black bg-opacity-70 w-full h-full absolute"></div>
+      <img src={url} alt={name} />
+      <h1 className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 text-white text-xl">
+        {name}
+      </h1>
+    </Link>
+  );
+};
+
+export default CardBidang;
--- app/ui/home/ListBidang.tsx
+++ app/ui/home/ListBidang.tsx
@@ -1,15 +1,31 @@
+import CardBidang from "@/app/components/CardBidang";
+
 const ListBidang = () => {
   return (
-    <div className="container w-full min-h-screen flex items-center">
-      <div className="w-full flex justify-center">
-        <div className="w-full text-center max-w-screen-sm">
-          <h1 className="text-2xl mb-5 md:text-4xl font-extrabold text-primary">
-            Bidang
-          </h1>
-          <p className="text-wrap">
-            OSIS SMK Telkom Makassar terbagi menjadi 10 bidang agar dapat fokus
-            menjalankan program kerja dan tugas masing-masing
-          </p>
+    <div className="container py-20 w-full min-h-screen flex items-center">
+      <div className="w-full">
+        <div className="w-full flex justify-center">
+          <div className="w-full text-center max-w-screen-sm">
+            <h1 className="text-2xl mb-5 md:text-4xl font-extrabold text-primary">
+              Bidang
+            </h1>
+            <p className="text-wrap">
+              OSIS SMK Telkom Makassar terbagi menjadi 10 bidang agar dapat
+              fokus menjalankan program kerja dan tugas masing-masing
+            </p>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 mt-5 sm:grid-cols-3 xl:grid-cols-5 gap-5">
+          <CardBidang name="Bidang 1" url="/images/bidang1.jpg" />
+          <CardBidang name="Bidang 2" url="/images/bidang2.jpg" />
+          <CardBidang name="Bidang 3" url="/images/bidang3.jpg" />
+          <CardBidang name="Bidang 4" url="/images/bidang4.jpg" />
+          <CardBidang name="Bidang 5" url="/images/bidang5.jpg" />
+          <CardBidang name="Bidang 6" url="/images/bidang6.jpg" />
+          <CardBidang name="Bidang 7" url="/images/bidang7.jpg" />
+          <CardBidang name="Bidang 8" url="/images/bidang8.jpg" />
+          <CardBidang name="Bidang 9" url="/images/bidang9.jpg" />
+          <CardBidang name="Bidang 10" url="/images/bidang10.jpg" />
         </div>
       </div>
     </div>
